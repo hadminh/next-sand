d516de7d50e0a8da2d6ffe24085a95dfcc666c58
hadminh <92121638+hadminh@users.noreply.github.com>
2021-10-27 13:40:26 -0400

    Initial commit

---
+++ pages/api/sffv.js
@@ -0,0 +1,28 @@
+import Cors from "cors";
+const cors = Cors({
+  methods: ["GET", "HEAD", "POST"]
+});
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+const algoliasearch = require("algoliasearch");
+const algoliaClient = algoliasearch(
+  "latency",
+  "6be0576ff61c053d5f9a3225e2a90f76"
+);
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
+  const { requests } = req.body;
+  const results = await algoliaClient.searchForFacetValues(requests);
+  res.status(200).send(results);
+}
